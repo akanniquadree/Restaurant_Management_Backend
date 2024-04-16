b3d9cb6dc4be580d816d2d45a886bbe5c48c5f22
akanniquadree <akanniquadry7@gmail.com>
2024-04-16 16:43:34 +0100

    Delete Category Api Updated

--- Routes/Category.js
+++ Routes/Category.js
@@ -1,99 +1,98 @@
-const express = require("express")
-const CategoryModel = require("../Model/CategoryModel")
-const ProductsModel = require("../Model/ProductModel")
-const { Verify, VerifyRole } = require("../MiddleWare/MiddleWare")
+const express = require("express");
+const CategoryModel = require("../Model/CategoryModel");
+const ProductsModel = require("../Model/ProductModel");
+const { Verify, VerifyRole } = require("../MiddleWare/MiddleWare");
 
-
-const categoryRouter = express.Router()
-
+const categoryRouter = express.Router();
 
 //Get all Category
-categoryRouter.get("/category", async(req, res)=>{
-    try {
-        const cat = await CategoryModel.find().populate("productId").sort("-createdAt")
-        if(cat){
-            return res.status(201).json(cat)
-        }
-            return res.status(422).json({error:"Error in getting category"})
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({error:error})
+categoryRouter.get("/category", async (req, res) => {
+  try {
+    const cat = await CategoryModel.find()
+      .populate("productId")
+      .sort("-createdAt");
+    if (cat) {
+      return res.status(201).json(cat);
     }
-})
+    return res.status(422).json({ error: "Error in getting category" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error });
+  }
+});
 
 //Create A Category
-categoryRouter.post("/category",Verify, VerifyRole, async(req, res)=>{
-    try {
-        const {name} = req.body
-        if(!name){
-            return res.status(422).json({error:"Please Fill all required field"})
-        }
-        const cat = new CategoryModel({name})
-        const savedCat = await cat.save()
-        if(savedCat){
-            return res.status(201).json({savedCat, message:"Category Added Successfully"})
-        }
-            return res.status(422).json({error:"Error in creating category"})
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({error:error})
+categoryRouter.post("/category", Verify, VerifyRole, async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(422).json({ error: "Please Fill all required field" });
     }
-})
+    const cat = new CategoryModel({ name });
+    const savedCat = await cat.save();
+    if (savedCat) {
+      return res
+        .status(201)
+        .json({ savedCat, message: "Category Added Successfully" });
+    }
+    return res.status(422).json({ error: "Error in creating category" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error });
+  }
+});
 
 //delete  Category
-categoryRouter.delete("/category/:id", Verify, VerifyRole,async(req, res)=>{
-    try {
-        const cat = await CategoryModel.findById(req.params.id)
-        const product = await ProductsModel.findOne({categoryId:req.params.id})
-        if(!cat){
-            return res.status(422).json({error:"Category cannot be found and updated"})
-        }
-        cat.deleteOne()
-        product.deleteMany()
-        if(savedCat){
-            return res.status(201).json({message:"Category Updated Successfully"})
-        }
-            return res.status(422).json({error:"Error in updating category"})
+// Verify, VerifyRole
+categoryRouter.delete("/category/:id", Verify, VerifyRole, async (req, res) => {
+  try {
+    const cat = await CategoryModel.findById(req.params.id);
 
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({error:error})
+    if (!cat) {
+      return res
+        .status(422)
+        .json({ error: "Category cannot be found and updated" });
     }
-})
+    // if (product) {
+    //   product.deleteMany({});
+    // }
+    if (cat) {
+      await cat.deleteOne();
+      await ProductsModel.deleteMany({
+        categoryId: req.params.id,
+      });
+      return res.status(201).json({ message: "Category Deleted Successfully" });
+    }
+    return res.status(422).json({ error: "Error in Deleting category" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error });
+  }
+});
 
 //Update A Category
-categoryRouter.put("/category/:id",Verify, VerifyRole, async(req, res)=>{
-    try {
-        const {name} = req.body
-        if(!name){
-            return res.status(422).json({error:"Please Fill all required field"})
-        }
-        const cat = await CategoryModel.findById(req.params.id)
-        if(!cat){
-            return res.status(422).json({error:"Category cannot be found and updated"})
-        }
-        cat.name = name
-        const savedCat = await cat.save()
-        if(savedCat){
-            return res.status(201).json({message:"Category Updated Successfully"})
-        }
-            return res.status(422).json({error:"Error in updating category"})
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({error:error})
+categoryRouter.put("/category/:id", Verify, VerifyRole, async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(422).json({ error: "Please Fill all required field" });
     }
-})
+    const cat = await CategoryModel.findById(req.params.id);
+    if (!cat) {
+      return res
+        .status(422)
+        .json({ error: "Category cannot be found and updated" });
+    }
+    cat.name = name;
+    const savedCat = await cat.save();
+    if (savedCat) {
+      return res.status(201).json({ message: "Category Updated Successfully" });
+    }
+    return res.status(422).json({ error: "Error in updating category" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-module.exports = categoryRouter
+module.exports = categoryRouter;
